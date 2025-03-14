15e40c73bfeb2abadc17f2ad51be719495a4dc7a
Nicolas Guerrero <nicolasguerrerop592@gmail.com>
2025-03-14 13:02:23 -0400

    se agrando el boton de iniciar sesion

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
         </div>
         <a
                 href="login-sesion/loginCliente.php"
-                class="px-6 py-2 bg-custom-blue-light text-white dark:bg-custom-blue dark:text-white rounded-full hover:bg-custom-blue dark:hover:bg-gray-600 transition-colors duration-200 font-semibold shadow-md"
+                class="px-8 py-3 text-lg bg-custom-blue-light text-white dark:bg-custom-blue dark:text-white rounded-full hover:bg-custom-blue dark:hover:bg-gray-600 transition-colors duration-200 font-semibold shadow-md"
             >
                 Iniciar Sesión
             </a>
