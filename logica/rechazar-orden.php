e9c5b7b8b89cdbcd5ee2689ed8541cea8629c611
Nicolas Guerrero <nicolasguerrerop592@gmail.com>
2025-03-16 01:22:50 -0400

    rechazar orden sirve

--- logica/rechazar-orden.php
+++ logica/rechazar-orden.php
@@ -2,20 +2,22 @@
 require 'conexionbdd.php';
 session_start();
 
-if (!isset($_SESSION['id']) || !isset($_GET['id'])) {
-    header('Location: ../panelAdmin/ordenes.php');
+if(!ISSET($_SESSION['id'])){
+    header('location:../login-sesion/login.php');
     exit();
 }
 
-$orden_id = $_GET['id'];
-$sql = "UPDATE ordenes SET estado = 'rechazada' WHERE id = ?";
+// Update the query to use id_orden instead of id
+$sql = "UPDATE ordenes SET estado = 'rechazada' WHERE id_orden = ?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $orden_id);
+$stmt->bind_param("i", $_GET['id']);
 
-if ($stmt->execute()) {
-    header('Location: ../panelAdmin/ordenes.php?mensaje=Orden rechazada exitosamente');
+if($stmt->execute()){
+    header('location: ../panelAdmin/ordenes.php?success=La orden ha sido rechazada');
 } else {
-    header('Location: ../panelAdmin/ordenes.php?error=Error al rechazar la orden');
+    header('location: ../panelAdmin/ordenes.php?error=Error al rechazar la orden');
 }
-exit();
+
+$stmt->close();
+$conn->close();
 ?>
